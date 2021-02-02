d83e331b308b0dd3a2b209854b50a89d07cc00a3
dreamxinxcode <dreamxinxcode@gmail.com>
2021-02-02 02:59:29 -0800

    image model

--- dropzone/models.py
+++ dropzone/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Image(models.Model):
+    image = models.ImageField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Images"
+
+    def __str__(self):
+        return self.image
